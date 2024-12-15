c06d898101e5a09390c0fefd4174a0b537e604b2
Morozqq <Aslan.dzheleubaj.04@gmail.com>
2024-12-15 21:44:32 +0500

    Fix table

---
+++ frontend/src/components/CustomerTable.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { defineProps } from 'vue'
+
+
+
+defineProps({
+    customer: Object
+})
+</script>
+
+<template>
+    <tbody>
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ customer.Number }}
+            </th>
+            <td class="px-6 py-4">
+                {{ customer.Name }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.Balance }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.CreditLimit }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.TotalSales }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.Costs }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.Address }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.Country }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.City }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.Phone }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.Email }}
+            </td>
+            <td class="px-6 py-4">
+                {{ customer.ContactName }}
+            </td>
+        </tr>
+
+    </tbody>
+</template>
---
+++ frontend/src/views/Customers.vue
@@ -0,0 +1,32 @@
+<script setup>
+import CustomerTable from '@/components/CustomerTable.vue'
+import CustomerData from '@/CustomerData.json'
+import { ref } from 'vue'
+
+const customers = ref(CustomerData)
+</script>
+
+<template>
+    <div class="relative overflow-x-auto container mx-auto">
+        <table class="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+            <thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">Number</th>
+                    <th scope="col" class="px-6 py-3">Name</th>
+                    <th scope="col" class="px-6 py-3">Balance</th>
+                    <th scope="col" class="px-6 py-3">Credit limit</th>
+                    <th scope="col" class="px-6 py-3">Total sales</th>
+                    <th scope="col" class="px-6 py-3">Costs</th>
+                    <th scope="col" class="px-6 py-3">Address</th>
+                    <th scope="col" class="px-6 py-3">Country</th>
+                    <th scope="col" class="px-6 py-3">City</th>
+                    <th scope="col" class="px-6 py-3">Phone</th>
+                    <th scope="col" class="px-6 py-3">Email</th>
+                    <th scope="col" class="px-6 py-3">Contact name</th>
+                </tr>
+            </thead>
+            <CustomerTable v-for:="customer in customers" :key="customer.id" :customer="customer" />
+        </table>
+    </div>
+
+</template>
