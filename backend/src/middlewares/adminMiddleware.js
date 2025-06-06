6951bc7ec0f7cd3a894c93656ee6a5ca9e617ff5
Arsen, Zhamshitov <arsen.zhamshitov@awara-it.com>
2025-06-06 20:29:09 +0500

    fix: fixed middlewares timeout

--- backend/src/middlewares/adminMiddleware.js
+++ backend/src/middlewares/adminMiddleware.js
@@ -1,19 +1,18 @@
 // adminMiddleware.js
 
-module.exports = function () {
-    return function (req, res, next) {
-        try {
-            const userRole = req.session.role;
-            if (!userRole) {
-                return res.status(403).json({ message: 'No role found. Access denied.' });
-            }
-            if (userRole !== 'admin') {
-                return res.status(403).json({ message: 'Access denied. Insufficient role.' });
-            }
-            next();
-        } catch (err) {
-            console.error('Role check failed:', err);
-            res.status(500).json({ message: 'Internal server error.' });
+module.exports = function (req, res, next) {
+    try {
+        const userRole = req.session.role;
+        if (!userRole) {
+            return res.status(403).json({ message: 'No role found. Access denied.' });
         }
-    };
+        if (userRole !== 'admin') {
+            return res.status(403).json({ message: 'Access denied. Insufficient role.' });
+        }
+        next();
+    } catch (err) {
+        console.error('Role check failed:', err);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
 };
+
