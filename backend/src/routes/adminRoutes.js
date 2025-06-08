e654c4447947e71437f790082b81332d113f62ce
Arsen, Zhamshitov <arsen.zhamshitov@awara-it.com>
2025-06-08 14:11:23 +0500

    fix route

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,9 +4,9 @@ const adminController = require('../controllers/adminController');
 const adminMiddleware = require('../middlewares/adminMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 
+router.get('/users/role', adminController.getCurrentUserRole);
 router.get('/users', authMiddleware, adminMiddleware, adminController.getAllUsers);
 router.get('/users/:id', authMiddleware, adminMiddleware, adminController.getUserById);
-router.get('/users/role', adminController.getCurrentUserRole);
 router.patch('/users/update/:id', authMiddleware, adminMiddleware, adminController.updateUser);
 router.delete('/users/delete/:id', authMiddleware, adminMiddleware, adminController.deleteUser);
 
